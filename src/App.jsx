bd09b3b61ceb33695506297067c612190a3660c0
val-maly <116696399+val-maly@users.noreply.github.com>
2022-11-10 08:41:17 -0700

    design cards section of the credit card form page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import './App.css';
+import cardLogo from './Images/card-logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      It works!!!
+    <div className='App'>
+      <header>
+        <div className='credit-card-back'>
+            <span className='credit-card-cvv'>000</span>
+        </div>
+        <div className='credit-card-front'>
+            <div className='credit-card-top'>
+              <img className='card-logo' src={cardLogo} alt='card-logo'/>
+            </div>
+            <div className='credit-card-middle'>
+              <span className='credit-card-number'>0000 0000 0000 0000</span>
+            </div>
+            <div class='credit-card-bottom'>
+              <span className='credit-card-name'>JANE APPLESEED</span>
+              <span className='credit-card-expiry'>09/00</span>
+            </div>
+        </div>
+      </header>
+      <main>
+      </main>
     </div>
   );
 }
